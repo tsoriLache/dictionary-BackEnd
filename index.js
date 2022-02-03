222a72aa13649ae56969d15b2d1b9aa78ea145ef
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-03 16:28:19 +0200

    random word by pos end-point

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const cors = require('cors');
-
+const DynamoDB = require('./dynamoDB/config');
+const posDict = require('./helpers/part-of-speech-dict');
 app.use(cors());
+
+// GET /part-of-speech/:pos - random word data in part-of-speech
+app.get('/part-of-speech/:pos', (req, res) => {
+  const { pos } = req.params;
+  //TODO validation and error handling
+  const params = {
+    TableName: 'dictionary',
+    FilterExpression: 'pos = :pos',
+    ExpressionAttributeValues: { ':pos': `${posDict[pos]}.` },
+  };
+  DynamoDB.scan(params, function (err, data) {
+    res.json(
+      err ? err : data.Items[Math.floor(Math.random() * data.Items.length)]
+    );
+  });
+});
 
 // GET /:word -  if word has more than one parts of speech will return all words part of speech, else, will return a word + definition + part of speech.
 app.get('/:word', (req, res) => {
