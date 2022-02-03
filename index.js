027495a099b1976666d1f621c86d2f9a220eb8b7
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-03 16:24:20 +0200

    basic get end point

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const cors = require('cors');
 
 app.use(cors());
 
+// GET /:word -  if word has more than one parts of speech will return all words part of speech, else, will return a word + definition + part of speech.
+app.get('/:word', (req, res) => {
+  const { word } = req.params;
+  //TODO validation
+  const params = {
+    TableName: 'dictionary',
+    KeyConditionExpression: 'word = :word',
+    ExpressionAttributeValues: { ':word': `${word.toUpperCase()}` },
+  };
+  DynamoDB.query(params, function (err, data) {
+    res.json(
+      err
+        ? err
+        : data.Count === 0
+        ? {}
+        : data.Count === 1
+        ? data.Items[0]
+        : data.Items.map((item) => item.pos)
+    );
+  });
+});
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
