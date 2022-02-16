4201a41a35c328adb66a723931532459a0bd8119
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-16 10:28:19 +0200

    partRouter

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const cors = require('cors');
 const wordRouter = require('./routers/wordRouter');
+const partRouter = require('./routers/partRouter');
 app.use(cors());
 
-app.get('/part-of-speech/:pos', (req, res) =>
-  req.query.letter ? randomByLetterAndPOS(req, res) : randomByPos(req, res)
-);
+app.use('/part-of-speech', partRouter);
 
 app.use('/', wordRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+// module.exports.handler = serverless(app);
